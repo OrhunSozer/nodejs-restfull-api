b2193b36de03b938a1f05c43347dd548cee19dc9
Orhun Sözer <orhun.sozer@gmail.com>
2021-05-10 19:49:36 +0300

    directory structure changed

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 let mongoose = require('mongoose');
+let morgan = require('morgan');
 let app = express();
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
+
+app.use(morgan('dev'));
 
 // Connect to Mongoose and set connection variable
 mongoose.connect('mongodb://localhost/nodejs-restfull-api', { useNewUrlParser: true});
@@ -21,11 +24,31 @@ else
 var port = process.env.PORT || 8080;
 
 
-// Import routes
-app.get('/', (req, res) => res.send('Hello World with Express'));
+//Error handling
+// app.use((req, res, next) =>{
+//     const error = new Error('Not found');
+//     error.status = 404;
+//     next(error);
+// })
 
-let routeContact = require("./routes/contact");
-let routeUser = require("./routes/user");
+app.use((error, req, res, next)=>{
+    console.log("error function:")
+    console.log(error);
+
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message: error.message
+        }
+    });
+})
+
+
+// Import routes
+app.get('/', (req, res) => res.send('Hello World with Express!'));
+
+let routeContact = require("./api/routes/contact");
+let routeUser = require("./api/routes/user");
 
 app.use('/api', routeContact);
 app.use('/api', routeUser);
